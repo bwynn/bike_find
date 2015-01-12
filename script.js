c5f2744d9a89e1655564aced79e274586ab28a87
Brian Wynn <brian.wynn83@gmail.com>
2015-01-11 16:00:34 -0800

    added initial array to script.js to start building out logic.

---
+++ script.js
@@ -0,0 +1,14 @@
+//rideFrequency
+//gathers user selection from checkbox
+var frequency = [
+  'every day',
+  '5 times a week',
+  '3 times a week',
+  'Once a week',
+  'Once a month',
+  'less than once a month'
+  ];
+// display returned value
+console.log(frequency[3]);
+
+//assigns value to bike selection function equation
